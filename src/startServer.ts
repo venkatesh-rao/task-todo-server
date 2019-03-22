a72360c83deae3840b5066ecc9241b54cf2bd81b
venkatesh-rao <venkatvr897@gmail.com>
2019-03-22 22:42:50 +0530

    Add express-session to store user related details

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,15 +1,38 @@
 import 'dotenv/config'
+import * as session from 'express-session'
 import { GraphQLServer } from 'graphql-yoga'
+import db from './models'
 import resolvers from './resolvers'
+
+const SESSION_SECRET = 'ajslkjalksjdfkl'
 
 export const startServer = async () => {
   const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: req => ({
-      ...req,
+    context: ({ request }) => ({
+      url: request.protocol + '://' + request.get('host'),
+      session: request ? (request as Express.Request).session : undefined,
+      req: request,
+      db,
     }),
   })
+
+  // session middleware
+  server.express.use(
+    session({
+      name: 'qid',
+      proxy: true,
+      secret: process.env.SESSION_SECRET || SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+      },
+    }),
+  )
 
   const cors = {
     credentials: true,
